4294d32986617c0d03d3162260abbadf67714ee4
Tola Tess <x17131553@student.ncirl.ie>
2019-10-23 19:49:12 +0100

    Player setting
    Player image and left and right movement

--- main.py
+++ main.py
@@ -11,6 +11,15 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('spaceicon.png')
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load('spaceship.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+def Player(x, y):
+    #blit - to draw
+    screen.blit(playerImg, (x, y))
 
 # Game Loop
 running = True
@@ -21,6 +30,24 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        # If keystroke is pressed check if right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -4.5
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 4.5
+                # If key released then stop player.
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+        #800 - 64 for the image size
+    elif playerX >= 736:
+        playerX = 736
+    Player(playerX, playerY) # will always show on the screen as in the while loop
     pygame.display.update() #NB always update your game window
 
 
